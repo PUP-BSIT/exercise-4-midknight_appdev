e5d6a97c95c3ed6a87a958472f213124ffe8539e
Jeff Carl Monroy <jeffcmonroy@gmail.com>
2024-04-30 18:31:02 +0800

    add create feature

--- task_tracker/src/app/parent/parent.component.ts
+++ task_tracker/src/app/parent/parent.component.ts
@@ -10,6 +10,13 @@ import { TaskService } from '../task.service';
 export class ParentComponent implements OnInit {
   tasks: Task[] = [];
 
+  newTask: Task = {
+    id: 0,
+    title: '',
+    description: '',
+    dueDate: new Date(),
+  };
+  selectedTask: Task | null = null;
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
@@ -18,5 +25,19 @@ export class ParentComponent implements OnInit {
 
   getTasks(): void {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  addTask(): void {
+    this.taskService.addTask(this.newTask);
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  clearForm(): void {
+    this.newTask = {
+      id: 0,
+      title: '',
+      description: '',
+      dueDate: new Date(),
+    };
   }
 }
